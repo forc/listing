eff4e220eb2e46efff56d803482851f224d68197
Pair <pair@basic.(none)>
2013-08-28 23:32:03 +0000

    Persist Country state and city

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,15 @@
 class HomeController < ApplicationController
   def index
     @countries = Country.all
+    @country_id = params[:country]
+    @state_id = params[:state]
+    @city_id = params[:city]
     @states = if params[:country]
       State.where(country_id: params[:country])
     else
       [] 
     end
+
     @cities = if params[:state]
       City.where(state_id: params[:state])
       else
